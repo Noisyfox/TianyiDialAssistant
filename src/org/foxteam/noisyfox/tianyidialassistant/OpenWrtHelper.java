445cec9b19c874042aee3a09798423e26be064cd
Noisyfox <timemanager.rick@gmail.com>
2014-09-19 18:21:23 +0800

    支持新版pppd

--- src/org/foxteam/noisyfox/tianyidialassistant/OpenWrtHelper.java
+++ src/org/foxteam/noisyfox/tianyidialassistant/OpenWrtHelper.java
@@ -210,18 +210,13 @@ public class OpenWrtHelper {
 		sb.append(" && ");
 		sb.append("uci set network.");
 		sb.append(wanInterface);
-		sb.append(".username=^#02");
+		sb.append(".username=tyxy#");
 		sb.append(phoneNumber);
-		sb.append("@njxy && ");
-		sb.append("uci set network.");
+		sb.append(" && uci set network.");
 		sb.append(wanInterface);
 		sb.append(".password=");
 		sb.append(psw);
 		sb.append(" && ");
-		sb.append("uci set network.");
-		sb.append(wanInterface);
-		sb.append(".pppd_options='ty_dial'");
-		sb.append("&&");
 		sb.append("uci commit");
 
 		final String cmd = sb.toString();
